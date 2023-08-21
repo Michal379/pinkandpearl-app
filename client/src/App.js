86560fb00351adb82d732fa620cd5c4abcf8e09e
Michal <khakaimic4@gmail.com>
2023-08-21 17:27:39 +0300

    home creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Note the import of Routes
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import WhatWeDo from './components/whatwedo/WhatWeDo';
+import ContactUs from './components/contactus/ContactUs';
+import WhoWeAre from './components/whoweare/WhoWeAre'; 
+import WhoWeArePage from './components/whoweare/WhoWeArePage';
+import OurProjects from './components/ourprojects/OurProjects';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/who-we-are" element={<WhoWeAre />} />
+          <Route path="/who-we-are" element={<WhoWeArePage />} />
+          <Route path="/what-we-do" element={<WhatWeDo />} />
+          <Route path="/our-projects" element={<OurProjects />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
