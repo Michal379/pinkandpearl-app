6a553001e3767d70027c0237cc26a037dd2c4099
Michal <khakaimic4@gmail.com>
2023-08-22 23:11:19 +0300

    navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; // If you're using React Router for navigation
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+        <li><Link to="/contact">Contact Us</Link></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
