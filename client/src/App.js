cac8c823afd936391c12b705d24ed30a887eae67
Michal <khakaimic4@gmail.com>
2023-08-23 13:23:39 +0300

    react app client creaton

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home'
-import AboutUs from './components/aboutus/AboutUs'
-import Services from './components/services/Services'
-import Gallery from './components/gallery/Gallery'
-import ContactUs from './components/contactus/ContactUs'
+import logo from './logo.svg';
+import './App.css';
 
-
-const App = () => {
+function App() {
   return (
-    <Router>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-      </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </Router>
-   
-  )
+  );
 }
 
-export default App
+export default App;
