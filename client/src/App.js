005e7931a468ef84c0d2e703692ec2811cfd7cdd
Michal <khakaimic4@gmail.com>
2023-08-24 17:33:02 +0300

    updated app.js for clear visibility

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import AboutUs from './components/aboutus/AboutUs';
 import Services from './components/services/Services';
 import Gallery from './components/gallery/Gallery';
-import ContactUs from './components/contactus/ContactUs'
+import ContactUs from './components/contactus/ContactUs';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-    </div>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
