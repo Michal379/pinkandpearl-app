01de2f0c26ba6c032650ba42805b18b0f8a91db5
Michal <khakaimic4@gmail.com>
2023-09-07 15:42:07 +0300

    made some modifications to the navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
         <img src={logoimage} alt="Logo" />
       </div>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-        <li><Link to="/contact">Contact Us</Link></li>
+      <li><Link to="/" className="nav-link">Home</Link></li>
+  <li><Link to="/about" className="nav-link">About Us</Link></li>
+  <li><Link to="/services" className="nav-link">Services</Link></li>
+  <li><Link to="/gallery" className="nav-link">Gallery</Link></li>
+  <li><Link to="/contact" className="nav-link">Contact Us</Link></li>
         <li className="book-now-button">
           <button>BOOK NOW</button>
         </li>
