60f0277946dd8acf9c60dfc9508c5137eead84e3
Michal <khakaimic4@gmail.com>
2023-09-08 08:36:33 +0300

    added the styling of the navbar logo and links

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -8,24 +8,26 @@ import logoimage from '../../images/logoimage.png';
 const Navbar = () => {
   return (
     <nav>
-      <div className='logo'>
-        <img src={logoimage} alt="Logo" />
+      <div className='container'>
+        <div className='logo'>
+          <img src={logoimage} alt="Logo" />
+        </div>
+        <ul>
+          <li><Link to="/">HOME</Link></li>
+          <li><Link to="/about">ABOUT US</Link></li>
+          <li><Link to="/services">SERVICES</Link></li>
+          <li><Link to="/gallery">GALLERY</Link></li>
+          <li><Link to="/contact">CONTACT US</Link></li>
+          <li className="book-now-button">
+            <button>BOOK NOW</button>
+          </li>
+          <li className="social-icons">
+            <a href="https://www.instagram.com/pinkandpearlwaxingbar/"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://www.facebook.com/profile.php?id=100085764224011"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="https://twitter.com/Pink_PearlBar"><FontAwesomeIcon icon={faTwitter} /></a>
+          </li>
+        </ul>
       </div>
-      <ul>
-      <li><Link to="/" className="nav-link">Home</Link></li>
-  <li><Link to="/about" className="nav-link">About Us</Link></li>
-  <li><Link to="/services" className="nav-link">Services</Link></li>
-  <li><Link to="/gallery" className="nav-link">Gallery</Link></li>
-  <li><Link to="/contact" className="nav-link">Contact Us</Link></li>
-        <li className="book-now-button">
-          <button>BOOK NOW</button>
-        </li>
-        <li className="social-icons">
-          <a href="https://www.instagram.com/pinkandpearlwaxingbar/"><FontAwesomeIcon icon={faInstagram} /></a>
-          <a href="https://www.facebook.com/profile.php?id=100085764224011"><FontAwesomeIcon icon={faFacebook} /></a>
-          <a href="https://twitter.com/Pink_PearlBar"><FontAwesomeIcon icon={faTwitter} /></a>
-        </li>
-      </ul>
     </nav>
   );
 }
