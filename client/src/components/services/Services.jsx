24660bb62c56e846d3b48bc86b385033565dcf20
Michal <khakaimic4@gmail.com>
2023-09-08 09:39:22 +0300

    added cards to the services page

--- client/src/components/services/Services.jsx
+++ client/src/components/services/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Services.css';
+import Card from 'react-bootstrap/Card';
 
 const Services = () => {
   const backgroundImageUrl = "https://saspacity.com/storage/images/styles/2020/07/01/image_1593619969373.jpg";
@@ -8,7 +9,47 @@ const Services = () => {
     <div className="background-image" style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
       <div className="services-container">
         <h1>Services</h1>
-        {/* Add your services content here */}
+        <div className="row">
+          {/* Card 1: Women Waxing */}
+          <div className="col-md-3">
+            <Card>
+              <Card.Body>
+                <Card.Title>WOMEN WAXING</Card.Title>
+                <Card.Text>Smooth and Confident - Wax with Pink & Pearl</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+
+          {/* Card 2: Manicure */}
+          <div className="col-md-3">
+            <Card>
+              <Card.Body>
+                <Card.Title>MANICURE</Card.Title>
+                <Card.Text>Polish Your Perfection</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+
+          {/* Card 3: Men Waxing */}
+          <div className="col-md-3">
+            <Card>
+              <Card.Body>
+                <Card.Title>MEN WAXING</Card.Title>
+                <Card.Text>Unleash Your Confidence</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+
+          {/* Card 4: Pedicure */}
+          <div className="col-md-3">
+            <Card>
+              <Card.Body>
+                <Card.Title>PEDICURE</Card.Title>
+                <Card.Text>Step into Bliss</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
   );
