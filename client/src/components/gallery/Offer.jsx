06e2681b8ea2d5dc2c08b5ffc2fd70a46161ddfc
Michal <khakaimic4@gmail.com>
2023-09-23 23:07:34 +0300

    adjusted the styling of the data on the offers page

--- client/src/components/gallery/Offer.jsx
+++ client/src/components/gallery/Offer.jsx
@@ -6,33 +6,38 @@ const Offer = () => {
     backgroundImage: `url(${image})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    backgroundAttachment: 'fixed', // Optional, makes the background fixed
-    minHeight: '60vh', // Optional, ensures the background covers the entire viewport height
-    // You can add more styles as needed
+    backgroundAttachment: 'fixed',
+    minHeight: '60vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center', // Center both horizontally and vertically
+    textAlign: 'center', // Center text within the container
+  };
+
+  const h1Style = {
+    position: 'relative',
+    lineHeight: '1.5',
+  };
+
+  const underlineStyle = {
+    position: 'absolute',
+    bottom: 0,
+    left: '50%',
+    transform: 'translateX(-50%)',
+    width: '30%',
+    borderBottom: '3px solid #000',
   };
 
   return (
     <div style={containerStyle}>
       <div>
-        {/* Content for your Offer component */}
         Our Special Offer
-        <h1 style={{
-  textAlign: 'center',
-  position: 'relative',
-  lineHeight: '1.5',
-}}>
-  YOU OWE YOURSELF THIS MOMENT
-  <span style={{
-    position: 'absolute',
-    bottom: 0,
-    left: '50%',
-    transform: 'translateX(-50%)',
-    width: '30%', // Adjust the width to control the underline length
-    borderBottom: '3px solid #000', // Use a 3px solid black underline
-  }}></span>
-</h1>
- 
-<button>MAKE AN APPOINTMENT</button>
+        <h1 style={h1Style}>
+          YOU OWE YOURSELF THIS MOMENT
+          <span style={underlineStyle}></span>
+        </h1>
+        <button>MAKE AN APPOINTMENT</button>
       </div>
     </div>
   );
