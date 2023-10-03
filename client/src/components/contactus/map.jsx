65da70727fe6b0c162c4125a73478d88079c5aa0
Michal <khakaimic4@gmail.com>
2023-10-04 00:00:26 +0300

    sparkles: added a map on the website

--- client/src/components/contactus/map.jsx
+++ client/src/components/contactus/map.jsx
@@ -21,15 +21,30 @@ const Map = () => {
 
   return (
     <div style={{ textAlign: 'center', marginTop: '40px' }}>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.257591077761!2d36.8204514!3d-1.2853467!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f117173d9a8f7%3A0x80601aef4f20c9ee!2sPink%20and%20Pearl%20Waxing%20%26%20Nail%20Bar!5e0!3m2!1sen!2ske!4v1695418454957!5m2!1sen!2ske"
-        width={mapWidth}
-        height={mapHeight}
-        style={{ border: 0 }}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+        <div style={{ flex: 1 }}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.257591077761!2d36.8204514!3d-1.2853467!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f117173d9a8f7%3A0x80601aef4f20c9ee!2sPink%20and%20Pearl%20Waxing%20%26%20Nail%20Bar!5e0!3m2!1sen!2ske!4v1695418454957!5m2!1sen!2ske"
+            width={mapWidth}
+            height={mapHeight}
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+        <div style={{ flex: 1 }}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.997901594314!2d36.9577758!3d-1.1608319!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f41fb9c1d4d4f%3A0x662a64c5069a8363!2sNairoview%20plaza!5e0!3m2!1sen!2ske!4v1696357443509!5m2!1sen!2ske"
+            width={mapWidth}
+            height={mapHeight}
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
       <p>Along Kenyatta Avenue At 680 Hotel Building, Second Floor RM 210, 680</p>
       <p>Hotel Building Is Directly Opposite I&M Bank Building</p>
     </div>
